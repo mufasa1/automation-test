82e0d23b1caaa93b95580996a599f5da10f792d9
jackwu19920718 <jackwu19920718@gmail.com>
2019-03-18 23:14:11 +0800

    Smoke version for propertySettingGet completed

--- src/main/java/TestClientIDs.java
+++ src/main/java/TestClientIDs.java
@@ -1,4 +1,6 @@
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Header;
+import com.jayway.restassured.response.Headers;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -7,12 +9,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.Format;
+import java.util.Formatter;
 
 public class TestClientIDs {
 
-    public final String domain_prefix = "https://chelliappqa701:";
+    public final String domain_prefix = "https://lips.lisqa7.sb.karmalab.net/LISService.svc";
 
-    public final String port = "8090";
+
 
     @Test
     public void smoke() {
@@ -24,7 +28,9 @@ public class TestClientIDs {
     public void PropertySettingGet(int clientID) {
         System.out.println(clientID);
         int houtelID = getValidHotelID();
-        RestAssured.when().get(domain_prefix + port).then().statusCode(200);
+        String propertySettingGetURL = String.format("/lips/V1/hotels/%s/propertysettings",houtelID);
+        Header head = new Header("Client-ID","1");
+        RestAssured.given().header(head).get(domain_prefix + propertySettingGetURL).then().statusCode(200);
     }
 
     private int getValidHotelID() {
@@ -37,18 +43,20 @@ public class TestClientIDs {
         return hotelID;
     }
     private boolean validateHotelIDInLSM(int hotelID) {
-        String sql = "select limID from dbo.skuGroupLIMPartition where hotelID=" + hotelID;
+        String sql = "select LIMPartitionID from dbo.skuGroupLIMPartition where skuGroupID=" + hotelID;
         String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-        String LSM_url = "";
-        String LSM_userName = "travel";
+        String LSM_url = "jdbc:sqlserver://LodgingSystemManagement.db.LISQA7.sb.karmalab.net;DatabaseName=LodgingSystemManagement";
+        String LSM_userName = "TravSysManagement";
         String LSM_password = "travel";
         try{
             Class.forName(driverName);
             Connection connection = DriverManager.getConnection(LSM_url,LSM_userName,LSM_password);
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            ResultSet resultSet = preparedStatement.executeQuery(sql);
-            if(resultSet.getInt("limID") !=0){
-                return true;
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                if(resultSet.getInt("LIMPartitionID") !=0) {
+                    return true;
+                }
             }
         }catch (Exception e){
             e.printStackTrace();
@@ -56,11 +64,11 @@ public class TestClientIDs {
         return false;
     }
 
-    private static int getHotelIDFromLim() {
+    private int getHotelIDFromLim() {
         int hotelID = 0;
-        String sql = "select top 1 skuGroupID from dbo.skugroup where activeStatus in (2,3) order by newID()";
+        String sql = "select top 1 skuGroupID from dbo.skugroup where ActiveStatusTypeID in (2,3) order by newID()";
         String driveName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-        String lim_url = "jdbc:sqlserver://lodginginventorytx1.db.LISQA7.sb.karmalab.net.LodgingInventoryMaster";
+        String lim_url = "jdbc:sqlserver://lodginginventorytx1.db.LISQA7.sb.karmalab.net;DatabaseName=LodgingInventoryMaster";
         String lim_userName = "TravInventory";
         String lim_password = "travel";
         try{
@@ -68,9 +76,12 @@ public class TestClientIDs {
             Connection dbConnect = DriverManager.getConnection(lim_url,lim_userName,lim_password);
             PreparedStatement statement = dbConnect.prepareStatement(sql);
             ResultSet resultSet = statement.executeQuery();
-            if(resultSet.getInt("skuGroupID") != 0){
-                hotelID = resultSet.getInt("skuGroupID");
+            while (resultSet.next()){
+                if(resultSet != null && resultSet.getInt("skuGroupID") != 0){
+                    hotelID = resultSet.getInt("skuGroupID");
+                }
             }
+
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -81,14 +92,9 @@ public class TestClientIDs {
     @DataProvider(name = "getClientID")
     public Object[][] getClienIDs(){
         return new Object[][]{
-                {1},
-                {2}
+                {1}
         };
     }
 
-    public static void main(String[] args){
-        int hotelID = getHotelIDFromLim();
-        System.out.println(hotelID);
-    }
 
 }
